9bc79e6fef8cf021498e5bfe8c02ff9e85fceaec
Padman <frimpongy890@gmail.com>
2023-09-08 00:53:42 +0000

    login page created

---
+++ lib/pages/login/login.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/custom_textfield.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          // color: Colors.amber,
+          constraints: const BoxConstraints(maxWidth: 420),
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Login',
+                    style: GoogleFonts.roboto(),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const CustomTextField(hintext: 'Email'),
+              const CustomTextField(
+                hintext: 'Password',
+                isPassword: true,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text('Login'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
