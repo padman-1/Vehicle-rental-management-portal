b6c32820f70bbb2bfb425b196cfd4ab5d364cabb
Padman <frimpongy890@gmail.com>
2023-10-22 14:38:32 +0000

    modified car details page

--- lib/pages/car_details_page.dart
+++ lib/pages/car_details_page.dart
@@ -5,10 +5,53 @@ class CarDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final devSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Car Details'),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: devSize.height / 2,
+                    width: devSize.width,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(image: AssetImage('assets/images/car1.png',
+                      ),
+                      fit: BoxFit.contain,
+                      ),
+                      color: Colors.blueGrey,
+                    ),
+                    // color: Colors.amber,
+                    // child: Image(image: AssetImage('assets/images/car1.png'),),
+                  ),
+                 const SizedBox(height: 20,),
+
+                  SizedBox(
+                    height: devSize.height/2,
+                    width: devSize.width,
+                    child: ListView.builder(
+                      itemCount: 8,
+                      itemBuilder: (context, index){
+                        return const ListTile(
+                          leading: Icon(Icons.location_city),
+                          title: Text('Milleage'),
+                          trailing: Text('250km'),
+                        );
+                      }),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
