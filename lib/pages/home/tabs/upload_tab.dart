a37b6c46f91ba558456f5263577d0003d57c8488
Padman <frimpongy890@gmail.com>
2023-10-22 14:40:01 +0000

    modified upload tab

--- lib/pages/home/tabs/upload_tab.dart
+++ lib/pages/home/tabs/upload_tab.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:vehicle_rental_management_portal/pages/car_upload/components/car_specs_form.dart';
 
 class UploadTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Upload'),
+    return const Center(
+      child: CarSpecsForm(),
     );
   }
 }
