0aa354762a647462c17775f7de9e81bb375897c4
Padman <frimpongy890@gmail.com>
2023-10-31 18:51:21 +0000

    created a default padding widget

---
+++ lib/widgets/default_paddings.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/cupertino.dart';
+
+class DefaultTextfieldPadding extends StatelessWidget {
+  const DefaultTextfieldPadding({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(height: 16,);
+  }
+}
