b00c7cff6f2d5e4aab9a7c53dce447009c8c0bd2
Padman <frimpongy890@gmail.com>
2023-10-31 18:52:21 +0000

    edited the car specs from textfield

--- lib/widgets/car_specs_form_textfield.dart
+++ lib/widgets/car_specs_form_textfield.dart
@@ -2,11 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CarSpecsFormTextField extends StatelessWidget {
-  const CarSpecsFormTextField({super.key, required this.icon, required this.labeltext, required this.hintext,  this.controller, this.maxLines=1,});
+  const CarSpecsFormTextField({super.key, required this.hintext,  this.controller, this.maxLines=1,});
 
   final String hintext;
-  final String labeltext;
-  final Icon icon;
+  
   final int maxLines;
   final TextEditingController? controller;
 
@@ -16,9 +15,8 @@ class CarSpecsFormTextField extends StatelessWidget {
       controller: controller,
       maxLines: maxLines,
       decoration: InputDecoration(
-        icon: icon,
         hintText: hintext,
-        labelText: labeltext,
+       
       ),
     );
   }
