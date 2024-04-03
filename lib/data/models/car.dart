f80210a47d41c26608fdd51dc22e2cd680965d7c
Padman <frimpongy890@gmail.com>
2024-04-03 23:40:42 +0000

    created car model

---
+++ lib/data/models/car.dart
@@ -0,0 +1,40 @@
+import 'dart:core';
+
+class Car {
+  String imgurl;
+  String descip;
+  String city;
+  // final features;
+  final String currency;
+  final String amount;
+  final String dur;
+  final String speed;
+  final String tankCapacity;
+  final String fuelLevel;
+  final String engineType;
+  final String millage;
+  final String power;
+  final String name;
+  final String brand;
+  final String vin;
+  final String insurance;
+
+  Car(
+      {required this.speed,
+      required this.tankCapacity,
+      required this.fuelLevel,
+      required this.engineType,
+      required this.millage,
+      required this.power,
+      required this.brand,
+      required this.vin,
+      required this.insurance,
+      required this.name,
+      required this.imgurl,
+      required this.descip,
+      required this.city,
+      // required this.features,
+      required this.amount,
+      required this.currency,
+      required this.dur});
+}
