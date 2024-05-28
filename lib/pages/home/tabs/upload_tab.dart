ba9386ba3d75af163a93058cb402256b89b1a2b9
Padman <frimpongy890@gmail.com>
2024-05-28 23:16:57 +0000

    added uploading specs to firestore

--- lib/pages/home/tabs/upload_tab.dart
+++ lib/pages/home/tabs/upload_tab.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vehicle_rental_management_portal/pages/car_upload/components/car_specs_form.dart';
 import 'package:vehicle_rental_management_portal/pages/car_upload/components/general_car_info_form.dart';
+import 'package:vehicle_rental_management_portal/pages/home/cubit/car_cubit/car_cubit.dart';
 
 class UploadTab extends StatefulWidget {
   @override
@@ -10,9 +12,25 @@ class UploadTab extends StatefulWidget {
 
 class _UploadTabState extends State<UploadTab>
     with SingleTickerProviderStateMixin {
+  var db = FirebaseFirestore.instance;
   late final _tabController = TabController(length: 2, vsync: this);
+  final _brandController = TextEditingController();
+  final _vehicleNumberController = TextEditingController();
+  final _insuranceNumberController = TextEditingController();
+  final _vinNumberController = TextEditingController();
+  final _amountController = TextEditingController();
+  final _carSpeedController = TextEditingController();
+  final _engineCapacityController = TextEditingController();
+  final _horsePowerController = TextEditingController();
+  final _engineTypeController = TextEditingController();
+  final _fuelLevelController = TextEditingController();
+  final _milleageController = TextEditingController();
+  final _tankCapacityController = TextEditingController();
+  bool isloading = false;
+
   @override
   Widget build(BuildContext context) {
+    final devSize = MediaQuery.of(context).size;
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -67,10 +85,65 @@ class _UploadTabState extends State<UploadTab>
                 controller: _tabController,
                 children: [
                   // first tab bar view widget
-                  GeneralCarInfoForm(),
+                  GeneralCarInfoForm(
+                    brandController: _brandController,
+                    vehicleNumberController: _vehicleNumberController,
+                    insuranceNumberController: _insuranceNumberController,
+                    vinNumberController: _vinNumberController,
+                    amountController: _amountController,
+                    selectedImage: '',
+                  ),
 
                   // second tab bar viiew widget
-                  CarSpecsForm()
+                  CarSpecsForm(
+                    button: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: isloading
+                          ? CircularProgressIndicator()
+                          : ElevatedButton(
+                              onPressed: () async {
+                                var docRef = db.collection("Cars").doc();
+                                final data = ({
+                                  "speed": _carSpeedController.text,
+                                  "tankCapacity": _tankCapacityController.text,
+                                  "fuelLevel": _fuelLevelController.text,
+                                  "engineType": _engineTypeController.text,
+                                  "millage": _milleageController.text,
+                                  "power": _horsePowerController.text,
+                                  "brand": _brandController.text,
+                                  "vin": _vinNumberController.text,
+                                  "insurance": _insuranceNumberController.text,
+                                  "name": '',
+                                  // "imgurl": '',
+                                  // "descip": description.text,
+                                  // "city": location.text,
+                                  "amount": _amountController.text,
+                                  "currency": 'Ghc',
+                                  // "dur": '1',
+                                  "carid": docRef.id,
+                                  // "ownerid": loggeduser.uid
+                                });
+                                setState(() {
+                                  isloading = true;
+                                });
+                                await docRef.set(data);
+                                setState(() {
+                                  isloading = false;
+                                });
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  foregroundColor: Colors.black),
+                              child: const Text('Upload')),
+                    ),
+                    carSpeedController: _carSpeedController,
+                    engineCapacityController: _engineCapacityController,
+                    horsePowerController: _horsePowerController,
+                    engineTypeController: _engineTypeController,
+                    fuelLevelController: _fuelLevelController,
+                    milleageController: _milleageController,
+                    tankCapacityController: _tankCapacityController,
+                  )
                 ],
               ),
             ),
