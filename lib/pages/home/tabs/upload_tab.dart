f5b6077bed31584973b2b9bdc977a83dd6cedc98
Padman <frimpongy890@gmail.com>
2024-07-30 21:39:25 +0000

    handled upload properly and showing snackbar once upload is done

--- lib/pages/home/tabs/upload_tab.dart
+++ lib/pages/home/tabs/upload_tab.dart
@@ -3,6 +3,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:uuid/uuid.dart';
 import 'package:vehicle_rental_management_portal/pages/car_upload/components/car_specs_form.dart';
 import 'package:vehicle_rental_management_portal/pages/car_upload/components/cubit/general_info_cubit.dart';
@@ -18,49 +19,18 @@ class _UploadTabState extends State<UploadTab>
     with SingleTickerProviderStateMixin {
   var db = FirebaseFirestore.instance;
   late final _tabController = TabController(length: 2, vsync: this);
-  final _brandController = TextEditingController();
-  final _vehicleNumberController = TextEditingController();
-  final _insuranceNumberController = TextEditingController();
-  final _vinNumberController = TextEditingController();
-  final _amountController = TextEditingController();
-  final _carSpeedController = TextEditingController();
-  final _engineCapacityController = TextEditingController();
-  final _horsePowerController = TextEditingController();
-  final _engineTypeController = TextEditingController();
-  final _fuelLevelController = TextEditingController();
-  final _milleageController = TextEditingController();
-  final _tankCapacityController = TextEditingController();
   String selectedImageUrl = '';
   bool isloading = false;
-
-  @override
-  void dispose() {
-    _amountController.dispose();
-    _brandController.dispose();
-    _carSpeedController.dispose();
-    _engineCapacityController.dispose();
-    _engineTypeController.dispose();
-    _vehicleNumberController.dispose();
-    _insuranceNumberController.dispose();
-    _vinNumberController.dispose();
-    _horsePowerController.dispose();
-    _fuelLevelController.dispose();
-    _milleageController.dispose();
-    _tankCapacityController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     final devSize = MediaQuery.of(context).size;
 
-    final specsState = context.read<SpecsInfoCubit>().state;
-
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             'Upload Car',
           ),
           centerTitle: true,
@@ -68,112 +38,98 @@ class _UploadTabState extends State<UploadTab>
           // elevation: 0,
           // leading: Text(''),
         ),
-        body: Column(
-          children: <Widget>[
-            // the tab bar with two items
-            SizedBox(
-              height: 50,
-              child: AppBar(
-                bottom: TabBar(
-                  controller: _tabController,
-                  tabs: const [
-                    Tab(
-                      text: 'General Car Information',
-                      //icon: Icon(Icons.directions_bike),
-                    ),
-                    Tab(
-                      text: 'Car Specifications',
-                      // icon: Icon(
-                      //   Icons.directions_car,
-                      // ),
-                    ),
-                  ],
+        body: BlocListener<SpecsInfoCubit, SpecsInfoState>(
+          listenWhen: (previous, current) =>
+              previous.specsFormState != current.specsFormState,
+          listener: (context, state) {
+            if (state.specsFormState == SpecsFormState.success) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text("Upload Successful"),
+                  backgroundColor: Colors.green,
+                ),
+              );
+            }
+          },
+          child: Column(
+            children: <Widget>[
+              // the tab bar with two items
+              SizedBox(
+                height: 50,
+                child: AppBar(
+                  bottom: TabBar(
+                    controller: _tabController,
+                    tabs: const [
+                      Tab(
+                        text: 'General Car Information',
+                        //icon: Icon(Icons.directions_bike),
+                      ),
+                      Tab(
+                        text: 'Car Specifications',
+                        // icon: Icon(
+                        //   Icons.directions_car,
+                        // ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
+              const SizedBox(
+                height: 10,
+              ),
 
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  // first tab bar view widget
-                  GeneralCarInfoForm(),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    // first tab bar view widget
+                    GeneralCarInfoForm(),
 
-                  // second tab bar viiew widget
-                  CarSpecsForm(
-                    button: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: isloading
-                          ? CircularProgressIndicator()
-                          : ElevatedButton(
+                    // second tab bar viiew widget
+                    CarSpecsForm(
+                      button: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Builder(builder: (context) {
+                          final specsFormState =
+                              context.select<SpecsInfoCubit, SpecsFormState>(
+                            (value) => value.state.specsFormState,
+                          );
+                          if (specsFormState == SpecsFormState.loading) {
+                            return const SizedBox(
+                              child: CircularProgressIndicator.adaptive(),
+                            );
+                          }
+                          return ElevatedButton(
                               onPressed: () async {
-                                Future Upload() async {
-                                  print('test-----' + 'htyfrhtf');
-                                  final id = const Uuid().v4();
-                                  final path = 'CarsImg/${id}';
-                                  final ref = FirebaseStorage.instance
-                                      .ref()
-                                      .child(path);
-                                  UploadTask uploadTask = ref.putData(
-                                      await specsState.imageFile!.readAsBytes(),
-                                      SettableMetadata(
-                                          contentType:
-                                              specsState.imageFile?.mimeType));
-                                  final snapshot =
-                                      await uploadTask.whenComplete(() => null);
-                                  selectedImageUrl =
-                                      await snapshot.ref.getDownloadURL();
-                                  print(selectedImageUrl);
-                                }
-
-                                var docRef = db.collection("Cars").doc();
-                                final data = ({
-                                  "speed": specsState.carSpeedController,
-                                  "tankCapacity":
-                                      specsState.tankCapacityController,
-                                  "fuelLevel": specsState.fuelLevelController,
-                                  "engineType":
-                                      specsState.engineCapacityController,
-                                  "millage": specsState.milleageController,
-                                  "power": specsState.horsePowerController,
-                                  "brand": specsState.brandController,
-                                  "vin": specsState.vinNumberController,
-                                  "insurance":
-                                      specsState.insuranceNumberController,
-                                  "name": '',
-                                  "imgurl": selectedImageUrl,
-                                  // "descip": description.text,
-                                  // "city": location.text,
-                                  "amount": specsState.amountController,
-                                  "currency": 'Ghc',
-                                  // "dur": '1',
-                                  "carid": docRef.id,
-                                  "status": "checkedIn",
-                                  // "ownerid": loggeduser.uid
-                                });
-                                setState(() {
-                                  isloading = true;
-                                });
-                                await docRef.set(data);
-                                setState(() {
-                                  isloading = false;
-                                });
+                                context.read<SpecsInfoCubit>().onSubmit();
                               },
                               style: ElevatedButton.styleFrom(
                                   backgroundColor: Colors.blue,
                                   foregroundColor: Colors.black),
-                              child: const Text('Upload')),
-                    ),
-                  )
-                ],
+                              child: const Text('Upload'));
+                        }),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future Upload(XFile image) async {
+    final specsState = context.read<SpecsInfoCubit>().state;
+
+    final id = const Uuid().v4();
+    final path = 'CarsImg/$id/${image.name}';
+    final ref = FirebaseStorage.instance.ref().child(path);
+    UploadTask uploadTask = ref.putData(await image.readAsBytes(),
+        SettableMetadata(contentType: image.mimeType));
+    final snapshot = await uploadTask.whenComplete(() => null);
+    selectedImageUrl = await snapshot.ref.getDownloadURL();
+    print(selectedImageUrl);
   }
 }
